e9e56158cb8bdc00e4ff0659e3426e334220cfe0
lu4nn4Gon <luanna1104@gmail.com>
2024-09-23 00:32:20 -0300

    txt com usuarios feito e teste de leitura

--- main.c
+++ main.c
@@ -4,30 +4,17 @@
 
 
 int main(void) {
-    char cpf[13];
-    char senha[11];
-
-    // abre o arquivo chamado usuarios.txt para adição de novo conteúdo
-    FILE *usuarios = fopen("usuarios.txt", "a");
-
-    printf("Digite seu CPF (apenas números, sem pontos ou traços): ");
-    fgets(cpf, 13 , stdin);
-    cpf[strcspn(cpf, "\n")] = '\0';
-
-    printf("Digite sua Senha: ");
-    fgets(senha, 11 , stdin);
-    senha[strcspn(senha, "\n")] = '\0';
-
-
-    if (senha[0] == '\0'){
-        printf("Campo Senha vazio");
-    } else if (cpf[0] == '\0') {
-        printf("Campo CPF vazio");
-    } else {
-        // grava no arquivo usuarios.txt
-        fprintf(usuarios, "%s - %s\n", cpf, senha);
+    FILE *usuarios;
+    int t = 256;
+    char linha[t];
+    // ler o arquivo txt
+    usuarios = fopen("usuarios.txt", "r");
+    while (!feof(usuarios)){
+       if (fgets(linha, t, usuarios) != NULL) {
+            printf("%s", linha);
+        }
     }
-
+    fgets(linha, t, usuarios);
     fclose(usuarios);
 
     return 0;
